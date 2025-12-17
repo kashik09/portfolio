6d37e4cd3d85fdff24c0ffab3ab39836a21604a7
kashik09 <kashiku789@gmail.com>
2025-12-17 15:59:05 +0300

    fix(auth): wrap main layout with SessionProvider

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { SessionProvider } from 'next-auth/react'
 
 export default function MainLayout({
   children,
@@ -7,12 +10,14 @@ export default function MainLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 container mx-auto px-6 md:px-8 lg:px-12 py-8">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <SessionProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 container mx-auto px-6 md:px-8 lg:px-12 py-8">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </SessionProvider>
   )
 }
